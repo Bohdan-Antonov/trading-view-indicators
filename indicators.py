d65dbfbbfccdcefecc1d5fc95818c9f009ce09f6
Bohdan Antonov <l.antonov.b@gmail.com>
2022-05-09 12:47:55 +0300

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -153,7 +153,7 @@ def standard_deviation(close_price, period):
 def bollinger_bands(close_price, period):
     upper_line = []
     lower_line = []
-    boll_bands = []
+    bands = []
     median = moving_average(close_price, period)
     std_dev = standard_deviation(close_price, period)
     if len(median) > len(std_dev):
@@ -163,11 +163,11 @@ def bollinger_bands(close_price, period):
     for i in range(len(std_dev)):
         upper_line.append(median[i] + (std_dev[i] * 2))
         lower_line.append(median[i] - (std_dev[i] * 2))
-        boll_bands.append({'median': median[i],
-                           'std dev': std_dev[i],
-                           'upper line': median[i] + (std_dev[i] * 2),
-                           'lower line': median[i] - (std_dev[i] * 2)})
-    return boll_bands
+        bands.append({'median': median[i],
+                      'std dev': std_dev[i],
+                      'upper line': median[i] + (std_dev[i] * 2),
+                      'lower line': median[i] - (std_dev[i] * 2)})
+    return bands
 
 
 # Donchian Channels
