efc059f4b70159199312d7681327f08aa6f34529
Bohdan Antonov <l.antonov.b@gmail.com>
2022-05-09 12:45:33 +0300

    Update ema.py

--- ema.py
+++ ema.py
@@ -3,9 +3,9 @@ import sma
 def exponential_moving_average(data, period):
     ma_list = sma.moving_average(data, period)
     multiplier = 2 / (period + 1)
-    ema_list = []
+    ema_values = []
     ema_start = (data[1] - ma_list[0]) * multiplier + ma_list[0]
-    ema_list.append(ema_start)
+    ema_values.append(ema_start)
     for i in range(len(data[2:])):
-        ema_list.append((data[i+1] - ema_list[i]) * multiplier + ema_list[i])
-    return ema_list
+        ema_values.append((data[i+1] - ema_values[i]) * multiplier + ema_values[i])
+    return ema_values
