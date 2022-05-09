a048162ba380fc33f345ff8e783d05e9e878a5ea
Bohdan Antonov <l.antonov.b@gmail.com>
2022-05-09 12:50:30 +0300

    Update rsi.py

--- rsi.py
+++ rsi.py
@@ -16,30 +16,3 @@ def relative_strength_index(close_price, period):
     for i in rsi:
         rsi_values.append(i)
     return rsi_values
-
-
-# Example
-
-ticker = 'BTCUSDT'
-interval = '1h'
-url = 'https://www.binance.com/api/v1/klines?symbol=BTCUSDT&interval=1h&limit=251'
-req = requests.get(url)
-
-
-inputs = {'open': [],
-          'high': [],
-          'low': [],
-          'close': [],
-          'volume': []}
-
-for i in req:
-    inputs['open'].append(float(i[1]))
-    inputs['high'].append(i[2])
-    inputs['low'].append(i[3])
-    inputs['close'].append(i[4])
-    inputs['volume'].append(i[5])
-
-
-df = pandas.DataFrame(inputs)
-rsi = relative_strength_index(df, 14)
-print(rsi)
